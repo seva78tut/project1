efdd94337faeae20a5900deef6e4f25db4865b20
Sergey <seva78@tut.by>
2021-04-15 23:20:32 +0300

    firsr commit

---
+++ 1script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+
--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-"use strict";
+'user strict';
+// let numberOfFilms = confirm ('Сколько фильмов вы уже посмотрели?');
+//console.log(numberOfFilms);
 
-const category =  'toys';
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?','');
 
-console.log(`http://someurl.com/${category}/5`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actros:{},
+    genres:[],
+    privat:"false",
+};
+//personalMovieDB [] = prompt ("",""),
+console.log(personalMovieDB);
 
-const user = 'Iban';
+const ask = prompt ("Один из посделних просмотренных фильмов?", " "),
+       ask1 = prompt ("На сколько оцените его?", " "),
+       ask2 = prompt ("Один из посделних просмотренных фильмов?", " "),
+       ask3 = prompt ("На сколько оцените его?", " ");
+ personalMovieDB.movies.ask = ask1;  
+ personalMovieDB.movies.ask2 = ask3;   
+ 
+ console.log(personalMovieDB);
 
-alert(`Привет, ${user}`);
+
+
+
+
