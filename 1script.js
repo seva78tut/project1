42e7771b166fe0aca03b0c173047c11c820d0f2d
Sergey <seva78@tut.by>
2021-04-18 19:22:10 +0300

    changes

--- 1script.js
+++ 1script.js
@@ -33,11 +33,50 @@ function showFirstMessage(text){
 showFirstMessage('Hello World ');
 console.log(num);
 
-function calc(a,b) {
-  return (a+b);
-}
 
-console.log(calc(4,3));
-console.log(calc(5,6));
-console.log(calc(10,6));
+
+function ret() {
+let num = 50;
+
+
+return num;
+}
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+  console.log('Hello ');
+};
+logger();
+
+const calc = (a, b) => {
+  console.log('1');
+  return a+b;
+};
+
+const str = 'teSt';
+//const arr = [1, 2, 5];
+
+//console.log(str[2] = 'd');
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+console.log(fruit.indexOf('q'));
+
+const logg = 'Hello world';
+
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num1 = 12.2;
+console.log(Math.round(num1));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
 
